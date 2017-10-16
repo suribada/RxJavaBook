c101fa19faf85e38b1848ec6904c43fc31d1cf93
suribada <suribada@korea.com>
2017-10-17 00:38:50 +0900

    add Learing Rxjava TestScheduler Test

---
+++ app/src/test/java/com/suribada/rxjavabook/LambdaTest.java
@@ -0,0 +1,25 @@
+package com.suribada.rxjavabook;
+
+import org.junit.Test;
+
+import io.reactivex.functions.Predicate;
+
+/**
+ * Created by lia on 2017-10-16.
+ */
+
+public class LambdaTest {
+
+    @Test
+    public void testLambda() {
+        Predicate<Integer> even = (number) -> number % 2 == 0;
+        boolean isEven = false;
+        try {
+            isEven = even.test(50);
+            System.out.println("isEven=" + isEven);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- app/src/test/java/com/suribada/rxjavabook/RxTest.java
+++ app/src/test/java/com/suribada/rxjavabook/RxTest.java
@@ -4,10 +4,14 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.TestScheduler;
 
 /**
  * Created by Noh.Jaechun on 2017. 3. 9..
@@ -59,5 +63,50 @@ public class RxTest {
 		Observable.fromIterable(list).filter(x -> x > 2).map(x -> x + 5).subscribe(observer);
 		observer.onNext(77);
 	}
+
+	@Test
+	public void testFuse() {
+		Observable.just(1, 2, 3, 4, 5, 6, 7)
+				.filter(x -> x > 1)
+				.filter(x -> x < 7)
+				.observeOn(Schedulers.io())
+				.subscribe(System.out::println);
+	}
+
+	@Test
+	public void schedulerTest() {
+		//Declare TestScheduler
+		TestScheduler testScheduler = new TestScheduler();
+
+		//Declare TestObserver
+		TestObserver<Long> testObserver = new TestObserver<>();
+
+		//Declare Observable emitting every 1 minute
+		Observable<Long> minuteTicker =
+				Observable.interval(1, TimeUnit.MINUTES,
+						testScheduler);
+
+		//Subscribe to TestObserver
+		minuteTicker.subscribe(testObserver);
+
+		//Fast forward by 30 seconds
+		testScheduler.advanceTimeBy(30, TimeUnit.SECONDS);
+
+		//Assert no emissions have occurred yet
+		testObserver.assertValueCount(1);
+
+		//Fast forward to 70 seconds after subscription
+		testScheduler.advanceTimeTo(70, TimeUnit.SECONDS);
+
+		//Assert the first emission has occurred
+		testObserver.assertValueCount(1);
+
+		//Fast Forward to 90 minutes after subscription
+		testScheduler.advanceTimeTo(90, TimeUnit.MINUTES);
+
+		//Assert 90 emissions have occurred
+		testObserver.assertValueCount(90);
+	}
+
 
 }
