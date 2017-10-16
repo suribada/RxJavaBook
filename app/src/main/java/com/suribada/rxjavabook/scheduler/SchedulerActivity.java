3562f65aeb799885eac0a9db92d0290611614e3c
suribada <suribada@korea.com>
2017-10-17 01:08:03 +0900

    add scheduer direct test

---
+++ app/src/main/java/com/suribada/rxjavabook/scheduler/SchedulerActivity.java
@@ -0,0 +1,52 @@
+package com.suribada.rxjavabook.scheduler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.suribada.rxjavabook.R;
+
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by lia on 2017-10-17.
+ */
+
+public class SchedulerActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_and_two_buttons);
+    }
+
+    public void onClickButton1(View view) {
+        Scheduler scheduler = Schedulers.io();
+        scheduler.scheduleDirect(() -> {
+            printInThread("first");
+        });
+        scheduler.scheduleDirect(() -> {
+            printInThread("second");
+        });
+    }
+
+    public void onClickButton2(View view) {
+        Scheduler scheduler = Schedulers.io();
+        Scheduler.Worker worker = scheduler.createWorker();
+        worker.schedule(() -> {
+            printInThread("third");
+        });
+        worker.schedule(() -> {
+            printInThread("fourth");
+        });
+    }
+
+
+    private void printInThread(String input) {
+        SystemClock.sleep(2000);
+        System.out.println(input + "Thread " + Thread.currentThread().getName());
+    }
+}
