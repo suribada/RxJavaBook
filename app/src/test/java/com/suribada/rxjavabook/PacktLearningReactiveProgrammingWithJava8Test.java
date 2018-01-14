302bbacfacebef219523463628380de475c0e5e7
suribada <suribada@korea.com>
2018-01-15 04:07:12 +0900

    GroupBy 샘플 추가

--- app/src/test/java/com/suribada/rxjavabook/PacktLearningReactiveProgrammingWithJava8Test.java
+++ app/src/test/java/com/suribada/rxjavabook/PacktLearningReactiveProgrammingWithJava8Test.java
@@ -12,14 +12,14 @@ import io.reactivex.Observable;
  */
 public class PacktLearningReactiveProgrammingWithJava8Test {
 
+    List<String> albums = Arrays.asList(
+            "The Piper at the Gates of Dawn", "A Saucerful of Secrets", "More",
+            "Ummagumma", "Atom Heart Mother", "Meddle", "Obscured by Clouds",
+            "The Dark Side of the Moon", "Wish You Were Here", "Animals",
+            "The Wall");
+
     @Test
     public void testGroupBy() {
-        List<String> albums = Arrays.asList(
-                "The Piper at the Gates of Dawn", "A Saucerful of Secrets", "More",
-                "Ummagumma", "Atom Heart Mother", "Meddle", "Obscured by Clouds",
-                "The Dark Side of the Moon", "Wish You Were Here", "Animals",
-                "The Wall");
-
         Observable.fromIterable(albums).groupBy(album -> album.split(" ").length)
                 .subscribe(obs -> {
                     obs.subscribe(v -> System.out.println(obs.getKey() + " word(s)"));
@@ -32,5 +32,12 @@ public class PacktLearningReactiveProgrammingWithJava8Test {
                 .subscribe(obs -> {
                     obs.subscribe(v -> System.out.println(obs.getKey() + " occurences of 'm' : " + obs));
                 });
+    }
+
+    @Test
+    public void testGroupBy2() {
+        Observable.fromIterable(albums).groupBy(album -> album.split(" ").length)
+                .flatMapSingle(obs -> obs.toList())
+                .subscribe(System.out::println);
     }
 }
