137bc9576c7cfa49e822d86968d2b79236a3cf4b
Noh.Jaechun <Naver@Navers-MacBook-Air-4.local>
2018-02-19 18:23:43 +0900

    ThreadActivity 추가

--- app/src/main/java/com/suribada/rxjavabook/chap1/TwoAsyncTaskActivity.java
+++ app/src/main/java/com/suribada/rxjavabook/chap1/TwoAsyncTaskActivity.java
@@ -1,10 +1,69 @@
 package com.suribada.rxjavabook.chap1;
 
-import Activity;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.widget.Button;
+
+import com.suribada.rxjavabook.R;
 
 /**
  * Created by Noh.Jaechun on 2018. 2. 5..
  */
 
 public class TwoAsyncTaskActivity extends Activity {
+
+    private boolean firstCondition, secondCondition;
+
+    private Button button;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_and_button);
+        button = (Button) findViewById(R.id.button);
+        button.setEnabled(false);
+        new AsyncTaskA().execute();
+        new AsyncTaskB().execute();
+    }
+
+    private class AsyncTaskA extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            SystemClock.sleep(3000);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            firstCondition = true;
+            enableButton();
+        }
+    }
+
+    private class AsyncTaskB extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            SystemClock.sleep(5000);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            secondCondition = true;
+            enableButton();
+        }
+    }
+
+    private void enableButton() {
+        if (firstCondition && secondCondition) {
+            button.setEnabled(true);
+        }
+    }
+
 }
