ba48db39c7e5fb62b13e4290254c667ef12f236a
suribada <suribada@korea.com>
2018-04-17 04:35:08 +0900

    CountDownLatch 테스트 추가

---
+++ app/src/main/java/com/suribada/rxjavabook/chap4/CountDownLatchActivitiy.java
@@ -0,0 +1,185 @@
+package com.suribada.rxjavabook.chap4;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.support.v4.os.AsyncTaskCompat;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.suribada.rxjavabook.R;
+import com.suribada.rxjavabook.api.model.Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by lia on 2018-04-17.
+ */
+public class CountDownLatchActivitiy extends Activity {
+
+    private static final String TAG = "CountDownLatch";
+
+    enum Type {
+        BEST, RECOMMEND, CATEGORY
+    }
+
+    private Map<Type, List<Book>> coverBooks = new HashMap<>();
+    private TextView title;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_and_two_buttons);
+        title = (TextView) findViewById(R.id.title);
+    }
+
+    private CountDownLatch latch;
+
+    public void onClickButton1(View view) {
+        latch = new CountDownLatch(2);
+        AsyncTaskCompat.executeParallel(new BestSellerAsyncTask());
+        AsyncTaskCompat.executeParallel(new RecommendAsyncTask());
+        AsyncTaskCompat.executeParallel(new CategoryAsyncTask(), 7);
+    }
+
+    private class BestSellerAsyncTask extends AsyncTask<Void, Void, List<Book>> {
+
+        @Override
+        protected List<Book> doInBackground(Void... voids) {
+            List<Book> books = new ArrayList<>();
+            try {
+                SystemClock.sleep(3000);
+                books.add(new Book(1, "채식주의자", "한강"));
+                books.add(new Book(2, "죄와벌", "도스토옙스키"));
+            } catch (Exception e) {
+                Log.d(TAG, "error", e);
+            }
+            return books;
+        }
+
+        @Override
+        protected void onPostExecute(List<Book> books) {
+            coverBooks.put(Type.BEST, books);
+        }
+    }
+
+    private class RecommendAsyncTask extends AsyncTask<Void, Void, List<Book>> {
+
+        @Override
+        protected List<Book> doInBackground(Void... voids) {
+            List<Book> books = new ArrayList<>();
+            try {
+                SystemClock.sleep(1000);
+                books.add(new Book(1, "저수지의 개들", "멍멍이"));
+                books.add(new Book(2, "안나 카레리나", "톨스토이"));
+            } catch (Exception e) {
+                Log.d(TAG, "error", e);
+            }
+            return books;
+        }
+
+        @Override
+        protected void onPostExecute(List<Book> books) {
+            coverBooks.put(Type.RECOMMEND, books);
+        }
+    }
+
+    private class CategoryAsyncTask extends AsyncTask<Integer, Void, List<Book>> {
+
+        @Override
+        protected List<Book> doInBackground(Integer... categories) {
+            int categoryId = categories[0];
+            List<Book> books = new ArrayList<>();
+            try {
+                SystemClock.sleep(5000);
+                books.add(new Book(7, "시험에 나오는 안드로이드", "노재춘"));
+                books.add(new Book(7, "발로 번역한 RxJava", "김인태"));
+            } catch (Exception e) {
+                Log.d(TAG, "error", e);
+            }
+            return books;
+        }
+
+        @Override
+        protected void onPostExecute(List<Book> books) {
+            coverBooks.put(Type.CATEGORY, books);
+        }
+    }
+
+    private void showBooks() {
+        
+    }
+
+    public void onClickButton2(View view) {
+        composedList.clear();
+        title.setText(null);
+        AsyncTaskCompat.executeParallel(new AsyncTaskA());
+        AsyncTaskCompat.executeParallel(new AsyncTaskB());
+    }
+
+    private ArrayList<String> composedList = new ArrayList<>();
+
+    private CountDownLatch latch2 = new CountDownLatch(1);
+
+    private class AsyncTaskA extends AsyncTask<Void, Void, List<String>> {
+
+        @Override
+        protected List<String> doInBackground(Void... params) {
+            SystemClock.sleep(3000);
+            return Arrays.asList("spring", "summer", "fall", "winter");
+        }
+
+        @Override
+        protected void onPostExecute(List<String> result) {
+            try {
+                composedList.addAll(result);
+                title.setText(TextUtils.join(", ", composedList));
+            } catch (Exception e) {
+                Toast.makeText(CountDownLatchActivitiy.this, "Error=" + e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                latch2.countDown();
+            }
+        }
+    }
+
+    private class AsyncTaskB extends AsyncTask<Void, Void, List<String>> {
+
+        @Override
+        protected List<String> doInBackground(Void... params) {
+            try {
+                SystemClock.sleep(1000);
+                //SystemClock.sleep(5000);
+                return Arrays.asList("east", "south", "west", "north");
+            } catch (Exception e) {
+                Log.d(TAG, "exception = " + e.getMessage());
+                return null;
+            } finally {
+                try {
+                    latch2.await();
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<String> result) {
+            if (result != null) {
+                composedList.addAll(result);
+                title.setText(TextUtils.join(", ", composedList));
+            }
+        }
+
+    }
+}
+
