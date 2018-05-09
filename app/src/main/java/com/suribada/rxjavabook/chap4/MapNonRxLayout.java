baf6edc7778bf8dbb6c7d7cfd2875f7b47cc994d
Noh.Jaechun <horseriding.king@navercorp.com>
2018-05-09 22:02:24 +0900

    리스너 인터페이스 대체

---
+++ app/src/main/java/com/suribada/rxjavabook/chap4/MapNonRxLayout.java
@@ -0,0 +1,62 @@
+package com.suribada.rxjavabook.chap4;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.suribada.rxjavabook.R;
+
+/**
+ * Created by Noh.Jaechun on 2018. 5. 9
+ */
+
+public class MapNonRxLayout extends FrameLayout implements View.OnClickListener {
+
+    public interface MapListener { // (1) 시작
+        void onZoomInClick();
+        void onZoomOutClick();
+        void onGpsActivationClick(); // (1) 끝
+    }
+
+    private View zoomIn, zoomOut, gpsActivation;
+    private MapListener mapListener;
+
+    public MapNonRxLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        inflate(context, R.layout.map_layout, this);
+        zoomIn = findViewById(R.id.zoom_in);
+        zoomOut = findViewById(R.id.zoom_in);
+        gpsActivation = findViewById(R.id.gps_activation);
+        zoomIn.setOnClickListener(this);
+        zoomOut.setOnClickListener(this);
+        gpsActivation.setOnClickListener(this);
+    }
+
+    public void setMapListener(@Nullable MapListener mapListener) { // (2) 시작
+        this.mapListener = mapListener;
+    } // (2) 끝
+
+    @Override
+    public void onClick(View v) {
+        if (mapListener == null) { // (3) 시작
+            return;
+        } // (3) 끝
+        switch (v.getId()) {
+            case R.id.zoom_in: // (4) 시작
+                mapListener.onZoomInClick();
+                break;
+            case R.id.zoom_out:
+                mapListener.onZoomOutClick();
+                break;
+            case R.id.gps_activation:
+                mapListener.onGpsActivationClick();
+                break; // (4) 끝
+        }
+    }
+
+}
---
+++ app/src/main/java/com/suribada/rxjavabook/chap4/MapRxLayout.java
@@ -0,0 +1,44 @@
+package com.suribada.rxjavabook.chap4;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.jakewharton.rxbinding2.view.RxView;
+import com.suribada.rxjavabook.R;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+
+/**
+ * Created by Noh.Jaechun on 2018. 5. 9
+ */
+
+public class MapRxLayout extends FrameLayout {
+
+    private View zoomIn, zoomOut, gpsActivation;
+
+    public MapRxLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        inflate(context, R.layout.books_main, this);
+        zoomIn = findViewById(R.id.zoom_in);
+        zoomOut = findViewById(R.id.zoom_in);
+        gpsActivation = findViewById(R.id.gps_activation);
+    }
+
+    public Observable<Object> getGpsActivationObservable() { // (1) 시작
+        return RxView.clicks(gpsActivation);
+    } // (1) 끝
+
+    public Observable<Integer> getZoomObservable() { // (2) 시작
+        Observable<Integer> zoomInObservable = RxView.clicks(zoomIn)
+                .map(Void -> Integer.valueOf(1));
+        Observable<Integer> zoomOutObservable = RxView.clicks(zoomOut)
+                .map(Void -> Integer.valueOf(-1));
+        return zoomInObservable.mergeWith(zoomOutObservable);
+    } // (2) 끝
+
+}
