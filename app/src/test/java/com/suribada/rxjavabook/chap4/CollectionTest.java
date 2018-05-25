a9db6ccc94f3ce010642fc638572655b6d682b43
Noh.Jaechun <horseriding.king@navercorp.com>
2018-05-25 22:00:47 +0900

    배열 테스트 추가

--- app/src/test/java/com/suribada/rxjavabook/chap4/CollectionTest.java
+++ app/src/test/java/com/suribada/rxjavabook/chap4/CollectionTest.java
@@ -2,6 +2,9 @@ package com.suribada.rxjavabook.chap4;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import io.reactivex.Observable;
 
 /**
@@ -10,10 +13,29 @@ import io.reactivex.Observable;
 public class CollectionTest {
 
     @Test
-    public void testCollection() {
-        Observable<Integer> firstObervable = Observable.just(1, 2, 4, 5, 7, 3);
-        Observable<Integer> secondObervable = Observable.just(1, 3, 5, 7, 9);
-        Observable<Integer> resultObservable = firstObervable.flatMap(x -> secondObervable,
+    public void testArray() {
+        Integer[] integers = new Integer[] {1, 2};
+        Observable.fromArray(integers)
+                .subscribe(System.out::println);
+        //primitive arrays are not supported
+        // https://stackoverflow.com/questions/43560993/rxjava-compile-error-on-the-following-case?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+        // https://github.com/ReactiveX/RxJava/issues/3518
+        int[] integers2 = new int[] {1, 2};
+        int[] integers3 = new int[] {3, 4};
+        Observable.fromArray(integers2, integers3) // 각 배열이 하나의 객체로 간주
+                .subscribe(System.out::println);
+
+    }
+    @Test
+    public void testJust() {
+        Integer[] integers = new Integer[] {1, 2};
+        Observable.fromArray(integers)
+                .subscribe(System.out::println);
+
+        Observable<Integer> firstObservable = Observable.just(1, 2, 4, 5, 7, 3);
+        Observable<Integer> secondObservable = Observable.just(1, 3, 5, 7, 9);
+
+        Observable<Integer> resultObservable = firstObservable.flatMap(x -> secondObservable,
                 (x, y) -> {
                     if (x == y) {
                         return Observable.just(x);
@@ -23,6 +45,21 @@ public class CollectionTest {
                 }).flatMap(x -> x);
         resultObservable.subscribe(System.out::println);
         resultObservable.toList().subscribe(System.out::println);
+    }
 
+    @Test
+    public void testCollection() {
+        List<Integer> firstList = Arrays.asList(1, 2, 4, 5, 7, 3);
+        List<Integer> secondList = Arrays.asList(1, 3, 5, 7, 9);
+        Observable.fromIterable(firstList)
+                .flatMap(x -> Observable.fromIterable(secondList),
+                    (x, y) -> {
+                        if (x == y) {
+                            return Observable.just(x);
+                        } else {
+                            return Observable.<Integer>empty();
+                        }
+                    }).flatMap(x -> x)
+                    .subscribe(System.out::println);
     }
 }
