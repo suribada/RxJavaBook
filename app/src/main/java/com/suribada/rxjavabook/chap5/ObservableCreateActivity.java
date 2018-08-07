02cfe3763c9f55f471c3d11f5638947e1699c2c6
Noh Jaechun <horseriding.king@navercorp.com>
2018-08-07 10:24:57 +0900

    클릭 이벤트 Observable  테스트 반영

--- app/src/main/java/com/suribada/rxjavabook/chap5/ObservableCreateActivity.java
+++ app/src/main/java/com/suribada/rxjavabook/chap5/ObservableCreateActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.sun.org.apache.xpath.internal.operations.String;
 import com.suribada.rxjavabook.R;
 
 import java.io.FileNotFoundException;
@@ -25,37 +24,49 @@ import io.reactivex.schedulers.Schedulers;
 public class ObservableCreateActivity extends Activity {
 
     private TextView title;
+    private View button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.four_buttons);
         title = findViewById(R.id.title);
-        try (
-            FileOutputStream fos = openFileOutput("book.txt", Context.MODE_PRIVATE)) {
-            PrintWriter out = new PrintWriter(fos);
+        button = findViewById(R.id.button1);
+        try (FileOutputStream fos = openFileOutput("book.txt", Context.MODE_PRIVATE);
+                PrintWriter out = new PrintWriter(fos)) {
             out.println("자바");
             out.println("안드로이드");
             out.println("RxJava");
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
-    private Disposable clickDisposable, fileDisposable;
-
-    public void onClickButton1(View view) {
-        clickDisposable = RxEventObservable.clicks(view)
+        clickDisposable = RxEventObservable.clicks(title)
                 .subscribe(ignored -> {
                     Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show();
                     title.setText("클릭 동작");
                 });
     }
 
-    public void onClickButton2(View view) {
+    private Disposable clickDisposable, fileDisposable;
+
+    public void onClickButton1(View view) {
         if (clickDisposable != null && !clickDisposable.isDisposed()) {
             clickDisposable.dispose();
         }
+    }
+
+    /**
+     * 다시 등록
+     *
+     * @param view
+     */
+    public void onClickButton2(View view) {
+        clickDisposable = RxEventObservable.clicks(title)
+                .subscribe(ignored -> {
+                    Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show();
+                    title.setText("클릭 동작");
+                });
     }
 
     public void onClickButton3(View view) {
