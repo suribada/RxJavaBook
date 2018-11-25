d60a6d8d6095c5956373fd8e85eadda84d5ab6ea
suribada <suribada@korea.com>
2018-11-26 05:22:23 +0900

    연산자 체인 시퀀스 다이어그램 추가

--- app/src/test/java/com/suribada/rxjavabook/chap5/SelectorTest.java
+++ app/src/test/java/com/suribada/rxjavabook/chap5/SelectorTest.java
@@ -1,5 +1,7 @@
 package com.suribada.rxjavabook.chap5;
 
+import com.suribada.rxjavabook.SystemClock;
+
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -10,7 +12,10 @@ import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.observers.TestObserver;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 
 import static org.junit.Assert.assertEquals;
 
@@ -66,6 +71,27 @@ public class SelectorTest {
         Thread.sleep(10000);
     }
 
+    /** 내부에서 PublishSubject를 사용하므로 이 내용은 아니다 */
+    @Test
+    public void publishWithSelector_internal_wrongAnswer() throws InterruptedException {
+        ConnectableObservable<Long> connectableObservable = Observable.interval(1000, TimeUnit.MILLISECONDS).publish();
+        connectableObservable.take(5)
+                .subscribe(value -> System.out.println("observer1=" + value));
+        SystemClock.sleep(8000);
+        connectableObservable.skip(3).subscribe(value -> System.out.println("observer2=" + value));
+        connectableObservable.connect();
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void publishWithSelector_internal() throws InterruptedException {
+        PublishSubject<Long> publishSubject = PublishSubject.create(); // (1)
+        Observable.interval(1000, TimeUnit.MILLISECONDS).subscribe(publishSubject); // (2)
+        publishSubject.take(5).concatWith(publishSubject.skip(3))
+                .subscribe(System.out::println);
+        Thread.sleep(10000);
+    }
+
     @Test
     public void publishWithSelector2() throws InterruptedException {
         Observable<Long> observable = Observable.interval(1000, TimeUnit.MILLISECONDS)
@@ -80,20 +106,30 @@ public class SelectorTest {
 
     @Test
     public void testTakeUntilWithPublishedStreamUsingSelector() throws InterruptedException {
-        Observable<Long> observable = Observable.interval(1000, TimeUnit.MILLISECONDS);
-        observable.publish(obs -> obs.take(7).concatWith(obs.takeUntil(obs.skipWhile(i -> i <= 3))))
-                .subscribe(System.out::println);
+        Observable<Long> observable = Observable.interval(1000, TimeUnit.MILLISECONDS)
+            .publish(obs -> obs.take(5).concatWith(obs.takeUntil(obs.skipWhile(i -> i <= 7))));
+        observable.subscribe(System.out::println);
         Thread.sleep(10000);
     }
 
     @Test
     public void replayWithSelector() throws InterruptedException {
-        Observable<Long> observable = Observable.interval(1000, TimeUnit.MILLISECONDS)
+        Observable<Long> observable = Observable.interval(1, TimeUnit.SECONDS)
                 .replay(obs -> obs.take(5).concatWith(obs.skip(3)));
         observable
                 .doOnNext(value -> System.out.println("observer1=" + System.currentTimeMillis()))
                 .subscribe(value -> System.out.println("observer1=" + value));
         Thread.sleep(10000);
+    }
+
+    @Test
+    public void replayWithSelector_internal() {
+        ConnectableObservable<Long> connectableObservable
+                = Observable.interval(1, TimeUnit.SECONDS).replay(); // (1)
+        connectableObservable.take(5).concatWith(connectableObservable.skip(3)) // (2)
+                .subscribe(System.out::println);
+        connectableObservable.connect(); // (3)
+        SystemClock.sleep(10000);
     }
 
     @Test
