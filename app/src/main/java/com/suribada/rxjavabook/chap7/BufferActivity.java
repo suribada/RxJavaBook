f40f2cb2e1f7381d39e201eace41bef1f9c9287a
suribada <suribada@korea.com>
2019-07-08 02:36:27 +0900

    버퍼 테스트 액티비티 추가

---
+++ app/src/main/java/com/suribada/rxjavabook/chap7/BufferActivity.java
@@ -0,0 +1,57 @@
+package com.suribada.rxjavabook.chap7;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.suribada.rxjavabook.R;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+public class BufferActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_and_three_buttons);
+        startInterval();
+    }
+
+    private Subject<Integer> boundarySubject = PublishSubject.create(); // (1)
+
+    void startInterval() {
+        Observable.interval(1, TimeUnit.SECONDS)
+                .take(20)
+                .buffer(boundarySubject) // (2)
+                .subscribe(buffer -> System.out.println(Thread.currentThread().getName() + ": " + buffer));
+    }
+
+    private void clickBoundary() {
+        boundarySubject.onNext(0); // (3)
+    }
+
+    public void onClickButton1(View view) {
+        clickBoundary();
+    }
+
+    public void onClickButton2(View view) {
+        Observable.interval(1, TimeUnit.SECONDS)
+                .take(20)
+                .buffer(() -> Observable.timer(5, TimeUnit.SECONDS, Schedulers.single())) // (1)
+                .subscribe(buffer -> System.out.println(Thread.currentThread().getName() + ": " + buffer));
+    }
+
+    public void onClickButton3(View view) {
+        Observable.interval(1, TimeUnit.SECONDS)
+                .take(20)
+                .buffer(Observable.interval(5, TimeUnit.SECONDS), // (1)
+                        value -> Observable.timer(value + 2, TimeUnit.SECONDS, Schedulers.single())) // (2)
+                .subscribe(buffer -> System.out.println(Thread.currentThread().getName() + ": " + buffer));
+    }
+}
