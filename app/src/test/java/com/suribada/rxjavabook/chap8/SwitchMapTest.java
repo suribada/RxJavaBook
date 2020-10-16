9b9960ba672ed7ac0923c7e359022c62c38aeed2
Noh.Jaechun <horseriding.king@navercorp.com>
2020-10-16 19:24:36 +0900

    switchOnNext 테스트

--- app/src/test/java/com/suribada/rxjavabook/chap8/SwitchMapTest.java
+++ app/src/test/java/com/suribada/rxjavabook/chap8/SwitchMapTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import io.reactivex.rxjava3.subjects.Subject;
@@ -21,7 +22,7 @@ public class SwitchMapTest {
     @Test
     public void switchMap_invalid() {
         Observable.just(2, 5, 17, 40)
-                .switchMap(v -> Observable.interval(v, TimeUnit.MILLISECONDS)
+                .switchMap(v -> Observable.interval(v, TimeUnit.MILLISECONDS) // (1)
                         .map(i -> (i + 1) * v + "밀리초 경과"))
                 .subscribe(System.out::println);
         SystemClock.sleep(3000);
@@ -57,9 +58,65 @@ public class SwitchMapTest {
 
     private Observable<SearchResult> searchApi(String keyword) {
         return Observable.create(emitter -> {
-            SystemClock.sleep(1000);
+            SystemClock.sleep(1500);
             emitter.onNext(new SearchResult(keyword));
         });
+    }
+
+    @Test
+    public void search_sameThread() {
+        getPopularKeywords()
+                .subscribeOn(Schedulers.io()) // (1)
+                .switchMap(keyword -> searchApi(keyword)) // (2
+                .subscribe(System.out::println);
+        SystemClock.sleep(10000);
+    }
+
+    @Test
+    public void search_differentThread() {
+        getPopularKeywords()
+                .subscribeOn(Schedulers.io())
+                .switchMap(keyword -> searchApi(keyword).subscribeOn(Schedulers.io()))
+                .subscribe(System.out::println);
+        SystemClock.sleep(10000);
+    }
+
+    private Observable<String> getPopularKeywords() {
+        return Observable.create(emitter -> {
+            emitter.onNext("자바");
+            SystemClock.sleep(1000);
+            emitter.onNext("코틀린");
+            SystemClock.sleep(1000);
+            emitter.onNext("안드로이드");
+            emitter.onComplete();
+        });
+    }
+
+    @Test
+    public void switchOnNext_invalid() {
+        Observable.switchOnNext(
+                Observable.fromArray( // (1) 끝
+                        Observable.interval(2, TimeUnit.SECONDS).map(v -> "2초:" + v),
+                        Observable.interval(3, TimeUnit.SECONDS).map(v -> "3초:" + v),
+                        Observable.interval(4, TimeUnit.SECONDS).map(v -> "4초:" + v)
+                )) // (1) 끝
+                .subscribe(System.out::println);
+        SystemClock.sleep(30000);
+    }
+
+    @Test
+    public void switchOnNext_valid() {
+        Observable.switchOnNext(
+                Observable.fromArray( // (1) 끝
+                        Observable.interval(2, TimeUnit.SECONDS).map(v -> "2초:" + v)
+                            .delaySubscription(10, TimeUnit.MILLISECONDS),
+                        Observable.interval(3, TimeUnit.SECONDS).map(v -> "3초:" + v)
+                            .delaySubscription(20, TimeUnit.MILLISECONDS),
+                        Observable.interval(4, TimeUnit.SECONDS).map(v -> "4초:" + v)
+                            .delaySubscription(30, TimeUnit.MILLISECONDS)
+                )) // (1) 끝
+                .subscribe(System.out::println);
+        SystemClock.sleep(30000);
     }
 
 
